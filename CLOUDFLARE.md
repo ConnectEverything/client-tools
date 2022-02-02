faee1a49f19397e7fdcc3cd20300232aa38a4b99
Phil Pennock <pdp@synadia.com>
2022-02-02 01:19:07 -0500

    Cloudflare process docs; nightlies stub script
    Documented a bunch of the web UI configuration done with CloudFlare.
    
    Have a script `build-nightlies.sh` which so far does exactly that, and checks
    that it has valid credentials to publish, but doesn't yet publish.  This
    provides a basis for working on the publishing side, so committing at this
    stage.

---
+++ CLOUDFLARE.md
@@ -0,0 +1,104 @@
+CloudFlare
+==========
+
+We are paying for the $200/month team for the "get-nats.io" website.
+
+Some notes
+
+## General Flow
+
+CloudFlare is linked to the client-tools git repository on GitHub, so gets
+told about every push, and will auto-deploy.  Deploys are slow to start as it
+takes 2 minutes to rebuild the running environment before they even clone the
+repo.
+
+We decouple the logic to build the site by running `./cdn-site-build` inside
+the repo, which generates `/public` (excluded from git) and the contents of
+that are deployed as a CF Pages site, `nats-tools.pages.dev`.
+
+One file within the area is `_redirects`, used so that requests to the root
+page issue a redirect to <https://www.synadia.com>.
+We can revisit this as needed.
+
+We have a domain `get-nats.io` registered through CF.
+In CF DNS, there is an "apex CNAME" (which gets flattened), pointing the
+domain at the `nats-tools.pages.dev` site.
+
+
+### www
+
+There is a rewrite rule in place for `www.get-nats.io` to redirect to `get-nats.io`;
+for client traffic to reach the rewrite rule, we need something in DNS.
+
+The `www` entry is an A pointing as CF-proxied to `192.0.2.1`, because as
+<https://community.cloudflare.com/t/redirecting-one-domain-to-another/81960>
+points out, we don't need a real backend here.
+
+(That's in the documentation/example reserved range.)
+
+So we configure 192.0.2.1 because it acts as a sentinel, we know something is
+up when we see it and no backend traffic will flow out from this proxied host;
+the **Rules** item for the site has the redirect.
+
+
+### misc DNS
+
+We enable DNSSEC.
+
+We let CF turn on the "no outbound mail" records.
+They were sane and what I was about to do manually.
+I added an MX of `0 .` to disable inbound email.
+The SRV support isn't flexible enough to let us publish `_client._smtp`.
+
+
+### misc other
+
+* SSL/TLS:
+  + Switched encryption from "Flexible" to "Full (Strict)"
+    - This affects how CF validates the connection to the origin server.
+    - Currently we're hosting entirely on CF so is a no-op, but given that
+    - We're serving installable assets and scripts, if that changes then we
+      want strict by default unless/until we decide otherwise
+  + Edge: enabled "Always use HTTPS"
+  + Edge: enabled HSTS, 6 months, not including sub-domains
+  + Edge: minimum TLS left on 1.0, the default
+    - While NATS itself might be fine with 1.2, we don't know if the people
+      installing will be behind corporate proxies, and so while I'm itching
+      to change this, I think the risk is high enough that for broadest
+      compatibility we leave this alone.
+    - When CF change the default, we should roll with it and accept the new
+      default
+  + Edge: TLS 1.3 (I don't remember if this defaulted on or I enabled it)
+  + Edge: CT monitoring, added `pdp+cf-ct@synadia.com` to receive notices
+
+* Network:
+  + HTTP/3 enabled
+  + WebSockets disabled (not used by us)
+  + other settings left on default (HTTP/2 on, IPv6 on, onion on, IP geo on
+    (`CF-IPCountry:` header)
+
+* Scrape Shield
+  + left all on defaults; the MIME restrictions etc look sane enough
+
+
+## Credentials
+
+Use API Tokens, **not** API Keys.  The Keys are legacy, per-user, full access.
+The tokens are scoped across both actions and targets.
+
+For development use visit <https://dash.cloudflare.com/profile/api-tokens> to
+create a token.  I've gone with a token having:
+
+    Synadia Communications - Workers R2 Storage:Edit, Workers KV Storage:Edit
+
+Since R2 storage isn't available without waitlists yet, and wants to know
+expected usage in TB, we're _probably_ not using that, but I've scoped my
+development to be able to access that just in case.
+
+
+There doesn't appear to be a fixed standard for what the environment variable
+should be called; the CF `wrangler` tool just writes it into
+`~/.wrangler/config/default.toml`.  The two common names found in searching
+are `CF_API_KEY` and `CLOUDFLARE_AUTH_TOKEN`.  I've gone with
+`$CLOUDFLARE_AUTH_TOKEN` for the nightlies script.
+
---
+++ build-nightlies.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+progname="$(basename "$0" .sh)"
+stderr() { printf >&2 '%s: %s\n' "$progname" "$*"; }
+warn() { stderr "$@"; }
+die() { warn "$@"; exit 1; }
+die_n() { local ev="$1"; shift; warn "$@"; exit "$ev"; }
+have_command() { command -v "$1" >/dev/null 2>&1 ; }
+readonly EX_USAGE=64 EX_SOFTWARE=70
+
+# We capture the initial pwd at the very beginning for reuse
+start_cwd="$(pwd)"
+readonly start_cwd
+
+# ========================================================================
+# CONFIG
+
+readonly BUILD_DIR='build'
+readonly SAFETY_CHECK_FILE='synadia-nats-channels.conf'
+readonly HTTP_USER_AGENT='client-tools-builder/0.1 (@philpennock, ConnectEverything)'
+
+# FIXME: do we need wrangler here
+readonly -a NEEDED_COMMANDS=(jq curl)
+
+# These will be set readonly at the end of parse_options:
+USE_EXISTING_BUILD=yes
+: "${NIGHTLY_DATE:=$(date +%F)}"
+
+declare -A tool_repo_slugs=(
+  [nats]='nats-io/natscli'
+  [nsc]='nats-io/nsc'
+)
+
+# ========================================================================
+# FUNCTIONS ONLY, NO DIRECT EXECUTION IN THIS SECTION
+
+usage() {
+  local ev="${1:-1}"
+  [[ $ev -eq 0 ]] || exec >&2
+  cat <<EOUSAGE
+Usage: $progname [-d <date>] [-f]
+  -d DATE     override date from ${NIGHTLY_DATE@Q}
+  -f          force build even if already exists
+  -P          don't publish (don't need API keys)
+
+If publishing, expect the credentials in: \$CLOUDFLARE_AUTH_TOKEN
+EOUSAGE
+  exit "$ev"
+}
+
+# SIDE-EFFECT: sets $opt_* and $parse_options_caller_shift
+# SIDE-EFFECT: updates $NIGHTLY_DATE
+# SIDE-EFFECT: updates $USE_EXISTING_BUILD
+parse_options() {
+  local arg OPTIND
+  opt_publish=1
+  while getopts ':d:fhP' arg; do
+    case "$arg" in
+      h) usage 0 ;;
+      d) NIGHTLY_DATE="$OPTARG" ;;
+      f) USE_EXISTING_BUILD='' ;;
+      P) opt_publish=0 ;;
+      :) die_n "$EX_USAGE" "missing required option for -$OPTARG; see -h for help" ;;
+      \?) die_n "$EX_USAGE" "unknown option -$OPTARG; see -h for help" ;;
+      *) die_n "$EX_SOFTWARE" "unhandled option -$arg; CODE BUG" ;;
+    esac
+  done
+  parse_options_caller_shift=$((OPTIND - 1))
+  shift "$parse_options_caller_shift"
+  if [[ $# -gt 0 ]]; then
+    die_n "$EX_USAGE" "unexpected command-line parameters"
+  fi
+
+  readonly USE_EXISTING_BUILD NIGHTLY_DATE
+}
+
+ua_curl() { command curl --user-agent "${HTTP_USER_AGENT:?}" "$@"; }
+cf_curl() { ua_curl -H "Authorization: Bearer $CLOUDFLARE_AUTH_TOKEN" -H "Content-Type: application/json" "$@"; }
+gh_curl() { ua_curl --user "${GITHUB_TOKEN}:x-oauth-basic" "$@"; }
+
+is_known_tool() { [[ -n "${tool_repo_slugs[$1]:+isset}" ]]; }
+require_known_tool() { is_known_tool "$1" || die "unknown tool ${1@Q}"; }
+
+dir_for_tool() {
+  printf '%s/%s/%s\n' "$start_cwd" "$BUILD_DIR" "$1"
+}
+
+fetch_one_github_repo() {
+  local tool="$1"
+  shift
+  local clone_dir repo_clone_url too_old_tag
+  cd "$start_cwd"
+  require_known_tool "$tool"
+  clone_dir="$(dir_for_tool "$tool")"
+  if [[ -d "$clone_dir/.git" ]]; then
+    # not the normal case, but handle it during dev
+    stderr "update in ${clone_dir@Q} (expect is github:${tool_repo_slugs[$tool]})"
+    git -C "$clone_dir" remote update -p
+    git -C "$clone_dir" merge --ff-only @{u}
+  else
+    stderr "clone github:${tool_repo_slugs[$tool]} -> ${clone_dir@Q}"
+    repo_clone_url="https://github.com/${tool_repo_slugs[$tool]}.git"
+    # Let's hope that version tags are only on the main branch;
+    # worst case scenario, we exclude based on a branch and clone more depth than optimal, which is acceptable.
+    too_old_tag="$(git ls-remote --tags --sort=-refname "$repo_clone_url" | grep -Fv '^' | grep -E 'refs/tags/(v|[0-9])' | head -n 3 | tail -n 1 | sed 's:^.*refs/tags/::')"
+    git clone \
+      --single-branch \
+      --shallow-exclude="$too_old_tag" \
+      "$repo_clone_url" "$clone_dir"
+  fi
+}
+
+# nsc overrides the dist dir from 'dist'.
+# Rather than parse all YAML, let's cheat for now and use sed.
+# If we need better parsing, it should only be in this function.
+dist_dir_for_tool() {
+  local tool="$1"
+  shift
+  local d clone_dir
+  clone_dir="$(dir_for_tool "$tool")"
+
+  dist_dir="$(sed -n 's/dist: *//p' < "${clone_dir}/.goreleaser.yml")"
+  [[ -n "$dist_dir" ]] || dist_dir='dist'
+
+  printf '%s/%s\n' "$clone_dir" "$dist_dir"
+}
+
+build_one_tool() {
+  local tool="$1"
+  shift
+  local clone_dir dist_dir
+  clone_dir="$(dir_for_tool "$tool")"
+  cd "$clone_dir"
+  if [[ -n "${SKIP_BUILD:-}" ]]; then
+    stderr "skipping build (per request) in ${clone_dir@Q}"
+    return
+  fi
+  dist_dir="$(dist_dir_for_tool "$tool")"
+  if [[ -f "${dist_dir}/artifacts.json" ]] && [[ -n "${USE_EXISTING_BUILD:-}" ]]; then
+    stderr "reusing existing build in ${clone_dir@Q}"
+    return
+  fi
+  goreleaser build --snapshot --rm-dist
+}
+
+check_have_publish_credentials() {
+  local verify label status
+  [[ -n "${CLOUDFLARE_AUTH_TOKEN:-}" ]] || die "missing content in \$CLOUDFLARE_AUTH_TOKEN (use -P to skip publish; -h for help)"
+  stderr 'checking $CLOUDFLARE_AUTH_TOKEN against CF verify end-point'
+  label='cloudflare[user/tokens/verify]'
+  verify="$(cf_curl -fSs https://api.cloudflare.com/client/v4/user/tokens/verify)" || die "$label failed"
+  jq <<<"$verify" -er .success >/dev/null || die "$label not successful $(jq <<<"$verify" -r '.errors[]')"
+  status="$(jq <<<"$verify" -er .result.status)"
+  [[ "$status" == "active" ]] || die "$label says \$CLOUDFLARE_AUTH_TOKEN is not active"
+  stderr 'creds okay'
+}
+
+# ========================================================================
+# MAIN FLOW
+
+main() {
+  local -i parse_options_caller_shift
+  local cmd tool
+  parse_options "$@"
+  shift "$parse_options_caller_shift"
+
+  [[ -f "$SAFETY_CHECK_FILE" ]] || die "bad starting dir? missing file ${SAFETY_CHECK_FILE@Q}"
+  for cmd in "${NEEDED_COMMANDS[@]}"; do
+    have_command "$cmd" || die "missing tool: ${cmd@Q}"
+  done
+  if (( opt_publish )); then
+    check_have_publish_credentials
+  fi
+
+  stderr "building $NIGHTLY_DATE"
+
+  [[ -d "$BUILD_DIR" ]] || mkdir -pv -- "$BUILD_DIR"
+
+  # We do not publish in the first loop, so that we have complete consistent sets
+  for tool in "${!tool_repo_slugs[@]}"; do
+    fetch_one_github_repo "$tool"
+    build_one_tool "$tool"
+  done
+  # Now we can publish
+  if (( opt_publish )); then
+    stderr "FIXME: publish here"
+  else
+    stderr "skipping publishing, per request"
+  fi
+
+  stderr "done"
+}
+
+# Don't run main if sourced; easier to test
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  main "$@"
+fi
